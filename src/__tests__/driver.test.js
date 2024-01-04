6cdff01fd6ecaa0bd6aca75444099536936576df
ikarofelix <ikaroisdm@gmail.com>
2024-01-04 14:37:30 -0300

    tests: improved driver and vehicle tests cases

--- src/__tests__/driver.test.js
+++ src/__tests__/driver.test.js
@@ -12,126 +12,114 @@ describe("/driver Endpoint", () => {
     expect(res.body.nome).toEqual("John Doe");
   });
 
+  it("should create multiple drivers", async () => {
+    const driver1 = { nome: "Driver 1" };
+    const driver2 = { nome: "Driver 2" };
+  
+    const response1 = await request(server).post("/driver").send(driver1);
+    const response2 = await request(server).post("/driver").send(driver2);
+  
+    expect(response1.status).toBe(201);
+    expect(response1.body).toMatchObject({ ...driver1, id: 2 });
+  
+    expect(response2.status).toBe(201);
+    expect(response2.body).toMatchObject({ ...driver2, id: 3 });
+  });
+
   it("should not create a driver without nome", async () => {
-    const res = await request(server)
-      .post("/driver")
-      .send({});
-    expect(res.statusCode).toEqual(400);
+    const driver = { idade: 30 };
+  
+    const response = await request(server).post("/driver").send(driver);
+  
+    expect(response.status).toBe(400);
+    expect(response.body.error).toEqual("Motorista inválido! Verifique se o campo nome foi preenchido corretamente.");
   });
 
-  it("should update a driver", async () => {
-    const res = await request(server)
-      .put("/driver/1")
-      .send({
-        nome: "John Doe Updated"
-      });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body.nome).toEqual("John Doe Updated");
+  it("should get all drivers", async () => {
+    const response = await request(server).get("/driver");
+  
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
   });
 
-  it("should not update a driver without placa, cor or marca", async () => {
-    const res = await request(server)
-      .put("/driver/1")
-      .send({});
-    expect(res.statusCode).toEqual(400);
-  });
-
-  it("should delete a driver", async () => {
-    const res = await request(server)
-      .delete("/driver/1");
-    expect(res.statusCode).toEqual(204);
-  });
-
-  it("should not delete a driver that does not exist", async () => {
-    const res = await request(server)
-      .delete("/driver/999");
-    expect(res.statusCode).toEqual(404);
-  });
-
-  it("should create multiple driver", async () => {
-    const drivers = [
-      { nome: "Joane Real" },
-      { nome: "Robert For" },
-      { nome: "Ed Domingues" },
-    ];
-
-    for (let driver of drivers) {
-      const res = await request(server)
-        .post("/driver")
-        .send(driver);
-      expect(res.statusCode).toEqual(201);
-      expect(res.body).toMatchObject(driver);
-    }
-  });
-
-  it("should get all driver besides deleted", async () => {
-    const res = await request(server)
-      .get("/driver");
-
-    for (let driver of res.body) {
-      expect(driver.id).not.toBe(1);
-    }
-  });
-
-  it("should recover a driver by id passing new nome", async () => {
-    const res = await request(server)
-      .put("/driver/1")
-      .send({
-        nome: "John Doe Updated"
-      });
-    expect(res.statusCode).toEqual(200);
-    expect(typeof res.body).toBe("object");
-  });
-
-  it("should delete a driver", async () => {
-    const res = await request(server)
-      .delete("/driver/1");
-    expect(res.statusCode).toEqual(204);
-  });
-
-  it("should recover a driver by id without passing new nome", async () => {
-    const res = await request(server)
-      .put("/driver/1");
-    expect(res.statusCode).toEqual(200);
-    expect(typeof res.body).toBe("object");
-  });
-
-  it("should not recover a driver by id", async () => {
-    const res = await request(server)
-      .put("/driver/999")
-      .send({
-        nome: "John Doe Updated"
-      });
-    expect(res.statusCode).toEqual(404);
-  });
-
-  it("should get all driver", async () => {
-    const res = await request(server)
-      .get("/driver");
-    expect(res.statusCode).toEqual(200);
-    expect(Array.isArray(res.body)).toBe(true);
-  });
-
-  it("should get all driver with filter for cor and marca", async () => {
-    const res = await request(server)
-      .get("/driver?nome=Robert&For");
-    expect(res.statusCode).toEqual(200);
-    expect(Array.isArray(res.body)).toBe(true);
-    expect(res.body.length).toEqual(1);
+  it("should get all drivers with a filter", async () => {
+    const nome = "Driver 2";
+    const response = await request(server).get(`/driver?nome=${nome}`);
+  
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    response.body.forEach(driver => {
+      expect(driver.nome).toBe(nome);
+    });
   });
 
   it("should get a driver by id", async () => {
-    const res = await request(server)
-      .get("/driver/1");
-    expect(res.statusCode).toEqual(200);
-    expect(typeof res.body).toBe("object");
+    const response = await request(server).get("/driver/1");
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(false);
   });
 
-  it("should not get a driver by id", async () => {
-    const res = await request(server)
-      .get("/driver/999");
-    expect(res.statusCode).toEqual(404);
-    expect(typeof res.body).toBe("object");
+  it("should not get a driver with an invalid id", async () => {  
+    const response = await request(server).get(`/driver/99999`);
+  
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual("Motorista com id 99999 não encontrado!");
+  });
+
+  it("should delete a driver by id", async () => {
+    const response = await request(server).delete(`/driver/1`);
+
+    expect(response.status).toBe(204);
+  });
+
+  it("should not delete a driver with an id that does not exist", async () => {
+    const response = await request(server).delete(`/driver/9999`);
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual("Motorista com id 9999 não encontrado!");
+  });
+
+  it("should update a driver by id", async () => {
+    const updatedData = { nome: "Updated Driver" };
+    const response = await request(server).put(`/driver/2`).send(updatedData);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({ ...updatedData, id: 2});
+  });
+
+  it("should not update a driver with an id that does not exist", async () => {
+    const updatedData = { nome: "Updated Driver" };
+    const response = await request(server).put(`/driver/9999`).send(updatedData);
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual("Motorista com id 9999 não encontrado!");
+  });
+
+  it("should not update a driver without nome", async () => {
+    const updatedData = {};
+    const response = await request(server).put(`/driver/2`).send(updatedData);
+
+    expect(response.status).toBe(400);
+    expect(response.body.error).toEqual("Motorista inválido! Verifique se os campo nome foi preenchido corretamente.");
+  });
+
+  it("should recover a driver by id that was deleted without nome", async () => {
+    const updatedData = {};
+    const response = await request(server).put(`/driver/1`).send(updatedData);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should recover a driver by id that was deleted with new nome", async () => {
+    const response1 = await request(server).delete(`/driver/1`);
+
+    expect(response1.status).toBe(204);
+
+    const updatedData = { nome: "Recovered Driver" };
+    const response2 = await request(server).put(`/driver/1`).send(updatedData);
+
+    expect(response2.status).toBe(200);
   });
 });
 
--- src/__tests__/vehicle.test.js
+++ src/__tests__/vehicle.test.js
@@ -7,138 +7,147 @@ describe("/vehicle Endpoint", () => {
       .post("/vehicle")
       .send({
         placa: "ABC-1234",
-        cor: "Blue",
+        cor: "Black",
         marca: "Toyota"
       });
     expect(res.statusCode).toEqual(201);
     expect(res.body.placa).toEqual("ABC-1234");
+    expect(res.body.cor).toEqual("Black");
+    expect(res.body.marca).toEqual("Toyota");
   });
 
-  it("should not create a vehicle without placa, cor or marca", async () => {
-    const res = await request(server)
-      .post("/vehicle")
-      .send({
-        placa: "ABC-1234",
-      });
-    expect(res.statusCode).toEqual(400);
+  it("should create multiple vehicles", async () => {
+    const vehicle1 = { placa: "ABC-1234", cor: "Black", marca: "Toyota" };
+    const vehicle2 = { placa: "XYZ-5678", cor: "White", marca: "Honda" };
+  
+    const response1 = await request(server).post("/vehicle").send(vehicle1);
+    const response2 = await request(server).post("/vehicle").send(vehicle2);
+  
+    expect(response1.status).toBe(201);
+    expect(response1.body).toMatchObject(vehicle1);
+  
+    expect(response2.status).toBe(201);
+    expect(response2.body).toMatchObject(vehicle2);
   });
 
-  it("should update a vehicle", async () => {
-    const res = await request(server)
-      .put("/vehicle/1")
-      .send({
-        placa: "XYZ-5678",
-        cor: "Red",
-        marca: "Honda"
-      });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body.placa).toEqual("XYZ-5678");
+  it("should not create a vehicle without placa", async () => {
+    const vehicle = { cor: "Black", marca: "Toyota" };
+  
+    const response = await request(server).post("/vehicle").send(vehicle);
+  
+    expect(response.status).toBe(400);
+    expect(response.body.error).toEqual("Veículo inválido! Verifique se os campos placa, cor e marca foram preenchidos corretamente.");
   });
 
-  it("should not update a vehicle without placa, cor or marca", async () => {
-    const res = await request(server)
-      .put("/vehicle/1")
-      .send({
-        placa: "XYZ-5678",
-      });
-    expect(res.statusCode).toEqual(400);
+  it("should get all vehicles", async () => {
+    const response = await request(server).get("/vehicle");
+  
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
   });
 
-  it("should delete a vehicle", async () => {
-    const res = await request(server)
-      .delete("/vehicle/1");
-    expect(res.statusCode).toEqual(204);
+  it("should get all vehicles with both filters", async () => {
+    const cor = "Black";
+    const marca = "Toyota";
+    const response = await request(server).get(`/vehicle?cor=${cor}&marca=${marca}`);
+  
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    response.body.forEach(vehicle => {
+      expect(vehicle.cor).toBe(cor);
+      expect(vehicle.marca).toBe(marca);
+    });
   });
 
-  it("should not delete a vehicle that does not exist", async () => {
-    const res = await request(server)
-      .delete("/vehicle/999");
-    expect(res.statusCode).toEqual(404);
+  it("should get all vehicles with cor filter", async () => {
+    const cor = "Black";
+    const response = await request(server).get(`/vehicle?cor=${cor}`);
+  
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    response.body.forEach(vehicle => {
+      expect(vehicle.cor).toBe(cor);
+    });
   });
 
-  it("should create multiple vehicle", async () => {
-    const vehicle = [
-      { placa: "ABC-1234", cor: "Blue", marca: "Toyota" },
-      { placa: "XYZ-5678", cor: "Red", marca: "Honda" },
-      { placa: "DEF-9012", cor: "Green", marca: "Ford" },
-    ];
-
-    for (let vehicle of vehicle) {
-      const res = await request(server)
-        .post("/vehicle")
-        .send(vehicle);
-      expect(res.statusCode).toEqual(201);
-      expect(res.body).toMatchObject(vehicle);
-    }
-  });
-
-  it("should get all vehicle besides deleted", async () => {
-    const res = await request(server)
-      .get("/vehicle");
-
-    for (let vehicle of res.body) {
-      expect(vehicle.id).not.toBe(1);
-    }
-  });
-
-  it("should recover a vehicle by id passing new placa, cor and marca", async () => {
-    const res = await request(server)
-      .put("/vehicle/1")
-      .send({
-        placa: "ABC-1234",
-        cor: "Blue",
-        marca: "Toyota"
-      });
-    expect(res.statusCode).toEqual(200);
-    expect(typeof res.body).toBe("object");
-  });
-
-  it("should delete a vehicle", async () => {
-    const res = await request(server)
-      .delete("/vehicle/1");
-    expect(res.statusCode).toEqual(204);
-  });
-
-  it("should recover a vehicle by id without passing new placa, cor and marca", async () => {
-    const res = await request(server)
-      .put("/vehicle/1");
-    expect(res.statusCode).toEqual(200);
-    expect(typeof res.body).toBe("object");
-  });
-
-  it("should not recover a vehicle by id", async () => {
-    const res = await request(server)
-      .put("/vehicle/999");
-    expect(res.statusCode).toEqual(404);
-  });
-
-  it("should get all vehicle", async () => {
-    const res = await request(server)
-      .get("/vehicle");
-    expect(res.statusCode).toEqual(200);
-    expect(Array.isArray(res.body)).toBe(true);
-  });
-
-  it("should get all vehicle with filter for cor and marca", async () => {
-    const res = await request(server)
-      .get("/vehicle?cor=red&marca=honda");
-    expect(res.statusCode).toEqual(200);
-    expect(Array.isArray(res.body)).toBe(true);
-    expect(res.body.length).toEqual(1);
+  it("should get all vehicles with marca filter", async () => {
+    const marca = "Toyota";
+    const response = await request(server).get(`/vehicle?marca=${marca}`);
+  
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    response.body.forEach(vehicle => {
+      expect(vehicle.marca).toBe(marca);
+    });
   });
 
   it("should get a vehicle by id", async () => {
-    const res = await request(server)
-      .get("/vehicle/1");
-    expect(res.statusCode).toEqual(200);
-    expect(typeof res.body).toBe("object");
+    const response = await request(server).get("/vehicle/1");
+  
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(false);
   });
 
-  it("should not get a vehicle by id", async () => {
-    const res = await request(server)
-      .get("/vehicle/999");
-    expect(res.statusCode).toEqual(404);
-    expect(typeof res.body).toBe("object");
+  it("should not get a vehicle with an invalid id", async () => {  
+    const response = await request(server).get(`/vehicle/99999`);
+  
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual("Veículo com id 99999 não encontrado!");
+  });
+
+  it("should delete a vehicle by id", async () => {
+    const response = await request(server).delete(`/vehicle/1`);
+
+    expect(response.status).toBe(204);
+  });
+
+  it("should not delete a vehicle with an id that does not exist", async () => {
+    const response = await request(server).delete(`/vehicle/9999`);
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual("Veículo com id 9999 não encontrado!");
+  });
+
+  it("should update a vehicle by id", async () => {
+    const updatedData = { placa: "AOW-9999", cor: "Red", marca: "Ford" };
+    const response = await request(server).put(`/vehicle/2`).send(updatedData);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({ ...updatedData, id: 2});
+  });
+
+  it("should not update a vehicle with an id that does not exist", async () => {
+    const updatedData = { placa: "AOW-9999", cor: "Red", marca: "Ford" };
+    const response = await request(server).put(`/vehicle/9999`).send(updatedData);
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual("Veículo com id 9999 não encontrado!");
+  });
+
+  it("should not update a vehicle without fields", async () => {
+    const updatedData = {};
+    const response = await request(server).put(`/vehicle/2`).send(updatedData);
+
+    expect(response.status).toBe(400);
+    expect(response.body.error).toEqual("Veículo inválido! Verifique se os campos placa, cor e marca foram preenchidos corretamente.");
+  });
+
+  it("should recover a vehicle by id that was deleted without fields", async () => {
+    const updatedData = {};
+    const response = await request(server).put(`/vehicle/1`).send(updatedData);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should recover a vehicle by id that was deleted with new fields", async () => {
+    const response1 = await request(server).delete(`/vehicle/1`);
+
+    expect(response1.status).toBe(204);
+
+    const updatedData = { placa: "AOW-9999", cor: "Red", marca: "Ford" };
+    const response2 = await request(server).put(`/vehicle/1`).send(updatedData);
+
+    expect(response2.status).toBe(200);
   });
 });
 
